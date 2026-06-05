d45b97487c4c93efabe95c0fb0c7ff961c24d19b
DrGrodus <ebermd00@gmail.com>
2026-06-05 13:30:06 -0500

    starting again

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.simple_clock.simple_clock {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.simple_clock.simple_clock to javafx.fxml;
-    exports org.simple_clock.simple_clock;
-}
--- src/main/java/org/simple_clock/simple_clock/HelloController.java
+++
@@ -1,4 +0,0 @@
-package org.simple_clock.simple_clock;
-
-public class HelloController {
-}

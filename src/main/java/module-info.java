b5c7a9c374af27311bc8d8cf1a3a9f463084424d
DrGrodus <ebermd00@gmail.com>
2026-06-12 17:01:32 -0500

    Trying new things from a tutorial, wondering why the function is called but the GUI of the app is never launched

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.example.simple_clock {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.desktop;
 
 
     opens org.example.simple_clock to javafx.fxml;
--- src/main/java/org/example/simple_clock/HelloController.java
+++ src/main/java/org/example/simple_clock/HelloController.java
@@ -1,57 +1,43 @@
 package org.example.simple_clock;
 
-import javax.swing.*;
+import javafx.fxml.FXML;
+
 import java.awt.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-public class HelloController extends JFrame {
-//    @FXML
-//    private Label timeLabel;
-    private JLabel timeLabel;
+public class HelloController {
+
+    @FXML
+    private Label timeLabel;
+
+    SimpleDateFormat timeFormat;
+    String time;
 
     public HelloController() {
-        setTitle("Hello!");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
-        setLocationRelativeTo(null);
-        setResizable(false);
+        timeFormat = new SimpleDateFormat("HH:mm:ss a");
 
-
-        JPanel centerPanel = new JPanel(new GridBagLayout()) {
-            // color
-        };
-        add(centerPanel);
-        centerPanel.add(timeLabel);
+        timeLabel = new Label();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setForeground(Color.cyan);
+        timeLabel.setBackground(Color.black);
+        startClock();
     }
 
+    public void startClock() {
+        do {
+            LocalTime currentTime = LocalTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+            String timeNow = currentTime.format(formatter);
+            time = timeNow;
+            timeLabel.setText(timeNow);
 
-    private void startClock(){
-//        AnimationTimer timer = new AnimationTimer() {
-//            @Override
-//            public void handle(long now) {
-//
-//            }
-//        };
-//        TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_LOCAL_TIME.parse(time.toString());
-//        Timer timer = new Timer(1000, e->updateClock());
-//        timer.start();
-        timeLabel = new JLabel();
-        timeLabel.setFont(new Font("Arial", Font.BOLD, 56));
-        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        Timer timer = new Timer(1000, e -> updateClock());
-        timer.start();
-
-        updateClock();
-    }
-
-    private void updateClock(){
-//        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss a");
-//        LocalTime time  = LocalTime.now();
-//        timeText = time.format(timeFormatter);
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-        String currentTime = formatter.format(new Date());
-        timeLabel.setText(currentTime);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (true);
     }
 }

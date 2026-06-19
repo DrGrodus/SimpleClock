2fdbe76d1515bf60530a5952eef919a6f02cc1db
DrGrodus <ebermd00@gmail.com>
2026-06-19 13:57:51 -0500

    First version finally working as it should

--- src/main/java/org/example/simple_clock/HelloController.java
+++ src/main/java/org/example/simple_clock/HelloController.java
@@ -1,8 +1,11 @@
 package org.example.simple_clock;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-
-import java.awt.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -19,25 +22,26 @@ public class HelloController {
         timeFormat = new SimpleDateFormat("HH:mm:ss a");
 
         timeLabel = new Label();
-        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
-        timeLabel.setForeground(Color.cyan);
-        timeLabel.setBackground(Color.black);
+        timeLabel.setFont(new Font("Verdana", 150));
+        timeLabel.setBackground(Background.fill(Color.BLACK));
         startClock();
     }
 
+    // Adapted from code of Kensoft PH https://kensoftph.com/how-to-get-current-time-in-java/
     public void startClock() {
-        do {
-            LocalTime currentTime = LocalTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
-            String timeNow = currentTime.format(formatter);
-            time = timeNow;
-            timeLabel.setText(timeNow);
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Thread thread = new Thread(() -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss a");
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                LocalTime currentTime = LocalTime.now();
+                time = currentTime.format(formatter);
+                Platform.runLater(() -> timeLabel.setText(time));
             }
-        } while (true);
+        });
+        thread.start();
     }
 }
